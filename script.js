8d08b41468cee0aeb9a4ebf49d056e0d84611c5a
Bukhari Auwal <idrisbukhariauwal@gmail.com>
2024-05-07 11:42:59 +0100

    introductionton tokdskl

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("im logging from external script");
+
+let myButton = document.getElementById("myButton");
+let number = 10;
+let name = "abba";
+
+myButton.addEventListener("click", () => {
+  console.log("i have been clicked externally");
+});
+
+console.log(animal);
+
+
