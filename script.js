d9479a963501a9a4addc98cb03e98d027da213f8
Aliyu-RSB <aliyusulaiman433@gmail.com>
2024-05-08 11:14:27 -0800

    const

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-console.log("im logging from external script");
+console.log("Hello How are you");
 
 let myButton = document.getElementById("myButton");
-let number = 10;
-let name = "abba";
+let num1 = 10;
+let num2 = 20;
+let num3 = num1 + num2;
+console.log(num3);
+
+let num4 = 10;
+let num5 = 20;
+let num6 = num1 / num2;
+console.log(num6);
+
+const email = "console@gmail.com"
+email ="aliyubichi@gmail.com"
 
 myButton.addEventListener("click", () => {
-  console.log("i have been clicked externally");
+  console.log(num3);
 });
-
-console.log(animal);
-
 
